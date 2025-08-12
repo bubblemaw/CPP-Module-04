05725a81851cd71b5d1a6cb6a26633b90b0250bf
Mawuli-Kplim Asase <masase@c2r5s9.42lausanne.ch>
2025-08-12 16:01:00 +0200

    working on ex03

--- ex03/AMateria.cpp
+++ ex03/AMateria.cpp
@@ -6,7 +6,33 @@
 /*   By: masase <masase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 16:11:09 by masase            #+#    #+#             */
-/*   Updated: 2025/08/11 16:27:11 by masase           ###   ########.fr       */
+/*   Updated: 2025/08/12 15:00:05 by masase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const & type) :_type(type)
+{
+    std::cout << "AMateria Constructor called" << std::endl;
+}
+
+AMateria::AMateria(const AMateria &obj): AMateria(obj._type)
+{
+    std::cout << "AMateria Copy Constructor called" << std::endl;
+    *this = obj;
+}
+
+AMateria::~AMateria()
+{
+    std::cout << "AMateria Destructor called" << std::endl;
+}
+
+AMateria &AMateria::operator=(const AMateria &obj)
+{
+    if (this != &obj)
+    {
+        this->_type = obj._type;
+    }
+    return (*this);
+}
--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -6,22 +6,28 @@
 /*   By: masase <masase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 16:11:06 by masase            #+#    #+#             */
-/*   Updated: 2025/08/11 16:12:46 by masase           ###   ########.fr       */
+/*   Updated: 2025/08/12 14:48:02 by masase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AMATERIA_HPP
 # define AMATERIA_HPP
+# include <string>
+# include <ostream>
+# include <iostream>
+
 class AMateria
 {
     protected:
-    //[...]
+        std::string const & _type;
     public:
-    AMateria(std::string const & type);
-    //[...]
-    std::string const & getType() const; //Returns the materia type
-    virtual AMateria* clone() const = 0;
-    virtual void use(ICharacter& target);
+        AMateria(std::string const & type);
+        ~AMateria();
+        AMateria(const AMateria &obj);
+        AMateria &operator=(const AMateria &obj);
+        std::string const & getType() const; //Returns the materia type
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
 };
 
 
